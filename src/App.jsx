341837a97d0e2eb6df7679c1df36bd9be8785b67
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-04-19 15:02:06 +0200

    Added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About";
 import Footer from "./components/Footer";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setIsLoaded(true);
+    });
+  }, []);
   return (
     <div className="bg-white/70">
-      <Header />
-      <About />
-      <Main />
-      <Footer />
+      {isLoaded ? (
+        <>
+          <Header />
+          <About />
+          <Main />
+          <Footer />
+        </>
+      ) : (
+        <>
+          <div className="w-full h-screen flex items-center justify-center text-4xl">
+            LOADING
+          </div>
+        </>
+      )}
     </div>
   );
 }
